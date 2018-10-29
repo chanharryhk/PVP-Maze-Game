2ec54fc99baded9f586fc90cc02173b6ff317ad6
Matthieu Roux <15mr59@queensu.ca>
2018-10-28 22:46:03 -0400

    went back

--- WindowsProject1/MazeConstructor.h
+++ WindowsProject1/MazeConstructor.h
@@ -8,7 +8,7 @@ class MazeConstructor {
 public:
 	MazeConstructor(int size);
 	int mazeSize;
-	vector< vector<MazeSquare> > maze;	//contains size x size matrix of MazeSquares to represent maze
+	vector < vector<MazeSquare>> maze;	//contains size x size matrix of MazeSquares to represent maze
 	int xStart;
 	int yStart;
 private:
