e6d7ad4df62b2485b460117b088326d81224a634
Leonard <16lz1@queensu.ca>
2018-10-31 20:47:55 -0400

    Added HumanPlayer classes; just bases so far

---
+++ WindowsProject1/HumanPlayer.cpp
@@ -0,0 +1,78 @@
+
+#include <iostream>
+#include <SFML\Graphics.hpp>
+
+#include "HumanPlayer.h"
+
+#define WINDOW_HEIGHT 300
+#define WINDOW_WIDTH 400
+
+Player::Player(const sf::Texture& imagePath) :
+                mSprite(imagePath),
+                mSource(1, Player::Down) {
+
+	// Sets the size of the sprite
+	mSprite.setTextureRect(sf::IntRect(mSource.x * 32, mSource.y * 32, 32, 32));
+}
+
+Player::~Player() {
+    // TODO Auto-generated destructor stub
+}
+
+void Player::draw(sf::RenderTarget& target, sf::RenderStates states) const {
+    target.draw(mSprite, states);
+}
+
+void Player::moveUp() {
+    mSource.y = Up;
+    if (mSprite.getPosition().y >= 0) {
+    		mSprite.move(0, -0.2);
+    }
+
+    //animation
+    mSource.x++;
+    if (mSource.x * 32 >= (int) mSprite.getTexture()->getSize().x) {
+        mSource.x = 0;
+    }
+}
+
+void Player::moveDown() {
+    mSource.y = Down;
+    if (mSprite.getPosition().y + mSprite.getLocalBounds().height <= WINDOW_HEIGHT) {
+    	mSprite.move(0, 0.2);
+    }
+
+    //animation
+    mSource.x++;
+    if (mSource.x * 32 >= (int) mSprite.getTexture()->getSize().x) {
+        mSource.x = 0;
+    }
+}
+
+void Player::moveLeft() {
+    mSource.y = Left;
+    if (mSprite.getPosition().x >= 0) {
+    	mSprite.move(-0.2, 0);
+    }
+
+    //animation
+    mSource.x++;
+    if (mSource.x * 32 >= (int) mSprite.getTexture()->getSize().x) {
+        mSource.x = 0;
+    }
+}
+
+void Player::moveRight() {
+    mSource.y = Right;
+    if (mSprite.getPosition().x + mSprite.getLocalBounds().width <= WINDOW_WIDTH) {
+    	mSprite.move(0.2, 0);
+    }
+
+    //animation
+    mSource.x++;
+    if (mSource.x * 32 >= (int) mSprite.getTexture()->getSize().x) {
+        mSource.x = 0;
+    }
+}
+
+
---
+++ WindowsProject1/HumanPlayer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <SFML\Graphics.hpp>
+
+class Player: public sf::Drawable {
+public:
+    enum Direction {
+        Down, Left, Right, Up
+    };
+
+    Player(const sf::Texture& playerTexture);
+    virtual ~Player();
+
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+private:
+    sf::Sprite mSprite;
+    sf::Vector2i mSource;
+};
