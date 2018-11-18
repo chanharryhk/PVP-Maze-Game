7c0d52b76ecad363aa3e8d264e6b6f2ef04fddb3
Harry <15hc40@caslab.queensu.ca>
2018-11-17 20:18:02 -0500

    Ready to merge

--- Group Assignment/src/items.h
+++ Group Assignment/src/items.h
@@ -10,44 +10,58 @@
 
 class Item {
 protected:
-	int location[1];
-	int scarcity;
+	// sf::SPrite mSprite;
+	int x; // Horizontal coordinate of the item
+	int y; // Vertical coordinate of the item
 	char color[10];
+	/* Visual representation of the rarity
+		[white]  - common
+		[blue]	 - rare
+		[purple] - epic
+		[orange] - legendary
+	*/
+	int itemScarcity;
+	// Player
 public:
+	// Default constructor
+	Item();
+	// Constructor
+	Item(const string& ability)
+	// Draws the sprite
+	// virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+	// Destructor
+	virtual ~Item();
+
+	// Ability of the item
 	void ability();
-	int location();
+
+	int getItemXPos() const;
+	int getItemYPos() const;
+	int getItemScarcity() const;
 };
 
 // Think about implementation of really rare boots kind of like fornite
 class Boots: public Item {
 public:
 	void ability() {
-// call on the player object and change the speed of
+	// call on the map object function and expand by a multiplier
 	}
-// srand (time(NULL));
-	int location(gameDimentionX, gameDimentionY) {
-// Unless both the dimentions are the same
-		for (int i = 0; i < scarcity; i++) {
-			location[0] = rand() % gameDimentionX;
-			location[1] = rand() % gameDimentionY;
-			return location;
-		}
-	}
+	// Replace with the ends of the maze
+	// Work with Joey or Mattieu on this
+	x = rand() % gameDimentionX;
+	y = rand() % gameDimentionY;
 };
 
 class Torch: public Item {
 public:
 	void ability() {
-// call on the map object function and expand by a multiplier
+	// call on the map object function and expand by a multiplier
 	}
-	int location(gameDimentionX, gameDimentionY) {
-// Unless both the dimentions are the same
-		for (int i = 0; i < scarcity; i++) {
-			location[0] = rand() % gameDimentionX;
-			location[1] = rand() % gameDimentionY;
-			return location;
-		}
-	}
+	// Replace with the ends of the maze
+	// Work with Joey or Mattieu on this
+	x = rand() % gameDimentionX;
+	y = rand() % gameDimentionY;
 };
 
 #endif /* ITEMS_H_ */
